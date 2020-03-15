87cfa3bdf761fdd7d7c697a5602123d4fece62bc
ilyasa Azmi <theazmi99ilyasa@gmail.com>
2020-03-15 10:07:25 +0700

    Initial Commit

---
+++ CoronaGis/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoronaGis
+//
+//  Created by Ilyasa Azmi on 15/03/20.
+//  Copyright © 2020 Ilyasa Azmi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
